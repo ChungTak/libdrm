3f7ad5c6d1e8a9971de685a8cc4d4c88cc8ff98f
ChungTak <chung-tak@qq.com>
2025-06-11 18:55:55 +0800

    修改 pkg-config 文件路径

--- build_with_zig.sh
+++ build_with_zig.sh
@@ -666,6 +666,8 @@ ninja -C $BUILD_DIR install
 # 检查安装结果
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}安装成功!${NC}"
+    # "修改 pkg-config 文件路径..."
+    find "${INSTALL_DIR}/lib/pkgconfig" -name "*.pc" -exec sed -i "s|^prefix=.*|prefix=/usr|g" {} \;    
     
     # 如果启用了大小优化，进行额外的压缩处理
     if [ "$OPTIMIZE_SIZE" = true ]; then
